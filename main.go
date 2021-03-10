f6ada5016f6c23c3435c2f07d25ad0fd871c7d7d
Thisura Samarakoon <thissamarakoon@gmail.com>
2021-03-10 16:16:03 +0530

    Created HelloWorld API

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe("localhost:3000", r)
+}
